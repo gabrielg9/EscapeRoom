8420bb696938bc6a0ebc78aaa20e0aa9c9074782
gabrielg9 <gabrielg9@interia.pl>
2019-09-22 23:00:30 +0200

    Added button in smartphone

--- Assets/Mobile_out.cs
+++ Assets/Mobile_out.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Mobile_out : MonoBehaviour
 {
+    public Button button;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
     }
 
     // Update is called once per frame
@@ -22,5 +25,9 @@ public class Mobile_out : MonoBehaviour
         GUI.Box(new Rect(0, 0, 200, 25), "Press 'Esc' to return to room");
         if (Input.GetKeyDown(KeyCode.Escape))
             SceneManager.LoadScene("EscapeRoom");
+    }
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
     }
 }
