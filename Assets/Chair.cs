3d06552e465e94647cee12c0efdb4d1b8095bd11
gabrielg9 <gabrielg9@interia.pl>
2019-10-02 22:36:11 +0200

    Added table with chairs
    Chair is moving after clicked E button, moving to start position when you went out from chair trigger

---
+++ Assets/Chair.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chair : MonoBehaviour
+{
+    public bool onTrigger;
+    [SerializeField] private Vector3 startPosition, movedPosition;
+    [SerializeField] private float animationTime;
+    [SerializeField] private Vector3 rotation, rotationBack;
+    private Hashtable iTweenArgs;
+
+    void Start()
+    {
+        iTweenArgs = iTween.Hash();
+        //iTweenArgs.Add("position", movedPosition);
+        //iTweenArgs.Add("rotation", rotation);
+        iTweenArgs.Add("time", animationTime);
+        iTweenArgs.Add("islocal", true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+    }
+
+    void Update()
+    {
+        if(onTrigger)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                iTweenArgs["position"] = movedPosition;
+                iTweenArgs["rotation"] = rotation;
+                moveChair();
+            }
+        }
+        else
+        {
+            iTweenArgs["position"] = startPosition;
+            iTweenArgs["rotation"] = rotationBack;
+            moveChair();
+        }
+        
+    }
+
+    private void moveChair()
+    {
+        iTween.MoveTo(gameObject, iTweenArgs);
+        iTween.RotateTo(gameObject, iTweenArgs);
+    }
+}
