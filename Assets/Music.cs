f933a51ccce3dde0010678855384f707a2b84f92
gabrielg9 <gabrielg9@interia.pl>
2019-10-06 23:08:55 +0200

    Add music to object
    Music start play when you are near object and you press "E" button.

---
+++ Assets/Music.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public bool onTrigger;
+    private AudioSource audioClip;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (onTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                audioClip = gameObject.GetComponent<AudioSource>();
+                audioClip.Play();
+            }
+        }
+    }
+}
