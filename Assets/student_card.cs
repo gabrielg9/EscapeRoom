44802217ae9e101d58840b3f55b6a6024bc88568
gabrielg9 <gabrielg9@interia.pl>
2019-10-16 17:49:53 +0200

    Added camera zooming on the student card

---
+++ Assets/student_card.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class student_card : MonoBehaviour
+{
+    public bool onTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(onTrigger)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Camera.main.fieldOfView = 30.0f;
+            }
+        }
+        else
+        {
+            Camera.main.fieldOfView = 60.0f;
+        }
+    }
+}
