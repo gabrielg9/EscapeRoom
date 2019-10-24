ad27656d8fb6a7301f402b1d5624445ee514b086
gabrielg9 <gabrielg9@interia.pl>
2019-10-24 12:54:45 +0200

    Added light switcher

---
+++ Assets/LightSwitcher.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitcher : MonoBehaviour
+{
+    public GameObject Switcher;
+    public GameObject Light;
+    private Light light;
+    public bool onTrigger;
+    private bool switched = false;
+    [SerializeField] private Vector3 rotation, returnRotation;
+    [SerializeField] private float animationTime;
+    private Hashtable iTweenArgs;
+
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+    }
+    void Start()
+    {
+        iTweenArgs = iTween.Hash();
+        iTweenArgs.Add("time", animationTime);
+        iTweenArgs.Add("islocal", true);
+        light = Light.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(onTrigger)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                switched = !switched;
+                if (switched)
+                {
+                    iTweenArgs["rotation"] = rotation;
+                    light.enabled = true;
+                }
+                else
+                {
+                    iTweenArgs["rotation"] = returnRotation;
+                    light.enabled = false;
+                }
+                    
+                switchLight();
+            }
+        }
+    }
+
+    private void switchLight()
+    {
+        iTween.RotateTo(Switcher, iTweenArgs);
+    }
+}
