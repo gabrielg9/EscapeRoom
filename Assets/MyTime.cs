d6b2b6fb85f91545ad231c2f2bf084fd42e9f0d4
gabrielg9 <gabrielg9@interia.pl>
2019-11-03 16:31:16 +0100

    Added timer

---
+++ Assets/MyTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyTime : MonoBehaviour
+{
+    private float allTime;
+    public static float realtimeSinceStartup;
+    public Text timerText;
+    private string textString;
+    int hours, minutes, seconds;
+    void Start()
+    {
+        allTime = Time.realtimeSinceStartup;
+        seconds = (int)(allTime % 60);
+        minutes = (int)(allTime / 60) % 60;
+        hours = (int)(allTime / 3600) % 24;
+    }
+
+    void Update()
+    {
+        textString = string.Format("{0:0}:{1:00}:{2:00}", hours, minutes, seconds);
+        timerText.text = "Twój czas wydostania się z pokoju to " + textString;
+    }
+}
