67d02fab35a3a7c3b6dac03341cdf43d90958824
gabrielg9 <gabrielg9@interia.pl>
2019-11-11 15:03:57 +0100

    Added radio audition

--- Assets/Music.cs
+++ Assets/Music.cs
@@ -6,8 +6,11 @@ public class Music : MonoBehaviour
 {
     public bool onTrigger;
     public bool listened = false;
-    private AudioSource audioClip;
+    private AudioSource music;
+    private AudioSource musicMessageCarpet;
     private bool popup = false;
+    public GameObject musicCarpet;
+    private int count;
     
     void OnTriggerEnter(Collider other)
     {
@@ -21,29 +24,46 @@ public class Music : MonoBehaviour
 
     void Start()
     {
-        
+        musicMessageCarpet = musicCarpet.GetComponent<AudioSource>();
+        music = gameObject.GetComponent<AudioSource>();
+        count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (onTrigger)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                musicMessageCarpet.Stop();
+                music.Stop();
                 popup = true;
-                audioClip = gameObject.GetComponent<AudioSource>();
-                audioClip.Play();
+                music.Play();
+                music.volume = 1.0f;
                 listened = true;
+                count++;
+                StartCoroutine(Wait());
             }
         }
         else
         {
             popup = false;
-            //audioClip.Stop();
+            //musicMessageCarpet.Stop();
+            //music.Stop();
         }
-            
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(10f);
+        music.Pause();
+        musicMessageCarpet.Play();
+        yield return new WaitForSeconds(28f);
+        count--;
+        if(count == 0)
+            music.UnPause();
+        music.volume = 0.03f;
     }
 
     private void OnGUI()
@@ -52,6 +72,5 @@ public class Music : MonoBehaviour
         {
             GUI.Box(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 15, 400, 30), "Upewnij się, że masz włączone głośniki lub podpięte słuchawki");
         }
-
     }
 }
